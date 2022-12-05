38c2ba8059b9cedf06f55d290ee984de9aa04ec8
kabikira <kabikirar2@protonmail.com>
2022-12-05 13:30:13 +0900

    Initial Commit

---
+++ TouchSynth/TouchSynthApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchSynthApp.swift
+//  TouchSynth
+//
+//  Created by koala panda on 2022/12/05.
+//
+
+import SwiftUI
+
+@main
+struct TouchSynthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
